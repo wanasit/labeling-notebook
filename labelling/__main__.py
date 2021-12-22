d9c883fcfccf8c74f8ce95adb21b0c27ad904808
Wanasit Tanakitrungruang <wanasit.wt@gmail.com>
2021-12-22 18:28:00 +0900

    Improve command line

--- labelling/__main__.py
+++ labelling/__main__.py
@@ -1,18 +1,44 @@
+import argparse
 import os
+import logging
+import webbrowser
 
+from threading import Timer
 from labelling.notebook import create_app
 
 module_dir = os.path.dirname(__file__)
 
+
 def main():
-    static_folder = os.path.join(module_dir, '../frontend/build')
-    instance_path = os.getcwd()
-    print(instance_path, static_folder)
-    debug = False
-    port = 9888
+    parser = argparse.ArgumentParser()
+    parser.add_argument('root', nargs='?', default=os.getcwd(),
+                        help="root directory for browsing files")
+
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--no-browser', action='store_true')
+    parser.add_argument('--port', default='9888', type=int,
+                        help="The port the labelling notebook server will listen on (default=9888)")
+    parser.add_argument('--ip', default='127.0.0.1', type=str,
+                        help="The IP address the labelling notebook server will listen on (default=127.0.0.1)")
+    parser.add_argument('--frontend', default=os.path.join(module_dir, '../frontend/build'),
+                        help="frontend built directory")
+
+    args = parser.parse_args()
+    run_notebook(args)
+
+
+def run_notebook(args):
     app = create_app(
-        instance_path=instance_path,
-        static_folder=static_folder
+        instance_path=args.root,
+        static_folder=args.frontend,
     )
 
-    app.run(debug=debug, port=port)
+    Timer(1, lambda: _try_open_browser(args)).start()
+    app.run(debug=args.debug, port=args.port, host=args.ip)
+
+
+def _try_open_browser(args):
+    if args.no_browser:
+        return
+
+    webbrowser.open_new(f'http://localhost:{args.port}')
